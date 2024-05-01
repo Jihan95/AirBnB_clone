a1578afebb115639b34d426cc890e70c45132a2a
MOHAMedelhennawy <el7ennaw2002@gmail.com>
2024-05-01 09:23:37 +0300

    reset

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,12 +24,24 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
+    classes = {
+            "BaseModel": BaseModel, "User": User, "City": City,
+            "Place": Place, "Review": Review, "Amenity": Amenity,
+            "State": State
+                }
 
-    def all(self):
+    def all(self, cls=None):
         """
         returns the dictionary __objects
         """
-        return FileStorage.__objects
+        if cls:
+            req_data = {}
+            for k, v in self.__objects.items():
+                if isinstance(v, cls):
+                    req_data[k] = v
+            return req_data
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """
@@ -57,17 +69,22 @@ class FileStorage:
         """
         deserializes the JSON file to __objects
         """
-        cls = {
-                "BaseModel": BaseModel, "User": User, "City": City,
-                "Place": Place, "Review": Review, "Amenity": Amenity,
-                "State": State
-                    }
+
         try:
             with open(self.__file_path, 'r') as file_obj:
                 data_dict = json.load(file_obj)
             for obj_dict in data_dict.values():
                 cls_name = obj_dict['__class__']
-                cls_name = cls[cls_name]
+                cls_name = self.classes[cls_name]
                 self.new(cls_name(**obj_dict))
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        to delete obj from __objects if it’s inside - if obj is equal to None,
+        the method should not do anything
+        """
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
--- models/state.py
+++ models/state.py
@@ -2,10 +2,13 @@
 """
 State class, a subclass of BaseModel
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.engine import file_storage
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """
     A subclass of BaseModel class
 
@@ -13,4 +16,11 @@ class State(BaseModel):
     name (str): state name
     """
 
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
+
+    @property
+    def cities(self):
+        all_cities = file_storage.FileStorage.all(self)
+        return all_cities
